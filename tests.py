bf69b8c44dfea50f7ad282bf21b8e6f7fab7cfb9
Charlie Shepherd <cshepherd@palantir.com>
2015-03-13 17:48:16 +0000

    Add more tests

--- tests.py
+++ tests.py
@@ -6,6 +6,8 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_timesort(self):
         self.assertEqual(timesort('5am', '6am'), -1)
+        self.assertEqual(timesort('6am', '5am'), 1)
+        self.assertEqual(timesort('10am', '1pm'), -1)
 
 if __name__ == '__main__':
     unittest.main()
