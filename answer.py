77d3ba28387701ba2affeafc211fe62955641d91
Charlie Shepherd <cshepherd@palantir.com>
2015-03-13 17:11:44 +0000

    Fix bug

--- answer.py
+++ answer.py
@@ -1,7 +1,7 @@
-match = re.search(r'.*what is the sum of (\\d+) and (\\d+)', s)
 def answer(q):
     if q == 'what is your name':
         return 'HungoverDeltas'
+    match = re.search(r'.*what is the sum of (\\d+) and (\\d+)', q)
     if match:
         return int(match.group(0)+match.group(1))
 
